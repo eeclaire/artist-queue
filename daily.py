300389588eccc99ac0df5844e5322f886ff6addc
Claire Durand <claire.durand@temple.edu>
2017-02-22 21:28:25 -0500

    move artist down queue if listened to recently

--- daily.py
+++ daily.py
@@ -9,6 +9,8 @@ import psycopg2.extras
 def main():
     """Main."""
     add_frequently_played_artists_to_queue()
+    move_recent_srobbles_artist_down_queue()
+    update_artist_set()
 
 
 def add_frequently_played_artists_to_queue():
@@ -22,8 +24,35 @@ def add_frequently_played_artists_to_queue():
                    from scrobbles
                    where scrobble_date>now()-interval '30' day
                    GROUP BY artist
-                   having count(*) >10
-                   and count(distinct song) > 8""")
+                   having count(*) > 15
+                   and count(distinct song) > 10""")
+
+    # Delete any older duplicates
+    cur.execute("""delete from artist_queue as l
+                   using artist_queue as r
+                   where l.artist = r.artist
+                   and l.id < r.id""")
+
+    # Make the changes persistent in the database and end communications
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def move_recent_srobbles_artist_down_queue():
+    """Push an artist down the queue if they've been listened to recently."""
+    conn = psycopg2.connect("dbname=artistqdb host=localhost user=postgres")
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # Find artists in queue who were played in the last day
+    # insert that artist with that most recent date in the queue
+    cur.execute("""insert into artist_queue (artist, last_scrobble_date)
+                   select s.artist,
+                          max(s.scrobble_date)
+                   from scrobbles s
+                   join artist_queue q on s.artist = q.artist
+                   where s.scrobble_date>now()-interval '1' day
+                   group by s.artist""")
 
     # Delete any older duplicates
     cur.execute("""delete from artist_queue as l
